62303fb4792effac8d599f6ace4c93c360d1b73a
RubTheKid <jadercardoso@tuta.io>
2025-09-05 22:10:39 -0300

    adding text fixtures

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,43 +1,48 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 // import { faker } from '@faker-js/faker';
 
-test('should create a new task', async ({ page, request }) => {
-    // const { faker } = await import('@faker-js/faker');
+test("should create a new task", async ({ page, request }) => {
+  // const { faker } = await import('@faker-js/faker');
 
-    //the new task that i want to include
-    // const taskName = faker.lorem.words();
-    const taskName = 'New Task from Playwright';
+  //the new task that i want to include
+  // const taskName = faker.lorem.words();
 
-    //in this page
-    await page.goto('http://localhost:8080');
+  const taskName = "New Task from Playwright";
 
-    //when i fill the input with the task
-    const inputTaskName = page.locator('#newTask');
-    await inputTaskName.fill(taskName);
-    //send the form/register the task
-    await page.click('css=button >> text=Create');
-    //i expect to see the task in the list
-    const target = page.locator('css=.task-item p >> text=', { hasText: taskName });
-    await expect(target).toBeVisible();
-    //clean up - delete the task
-    // await request.delete(`http://localhost:3333/helper/tasks/${taskName}`);
+  await request.delete(`http://localhost:3333/helper/tasks/${taskName}`);
+  //in this page
+  await page.goto("http://localhost:8080");
+
+  //when i fill the input with the task
+  const inputTaskName = page.locator("#newTask");
+  await inputTaskName.fill(taskName);
+  //send the form/register the task
+  await page.click("css=button >> text=Create");
+  //i expect to see the task in the list
+  const target = page.locator("css=.task-item p >> text=", {
+    hasText: taskName,
+  });
+  await expect(target).toBeVisible();
+  //clean up - delete the task
+  // await request.delete(`http://localhost:3333/helper/tasks/${taskName}`);
 });
 
-test('should not create a task that exists', async({ page}) => {
-    const taskName = 'New Task from Playwright';
-    await page.goto('http://localhost:8080');
+test("should not create a task that exists", async ({ page, request }) => {
+  const taskName = "New Task from Playwright";
+  await page.goto("http://localhost:8080");
 
-    const inputTaskName = page.locator('#newTask');
-    await inputTaskName.fill(taskName);
+  const inputTaskName = page.locator("#newTask");
+  await inputTaskName.fill(taskName);
 
-    
-    await page.click('css=button >> text=Create'); 
+  await page.click("css=button >> text=Create");
 
-    // const target = page.locator('.swal2-html-container >> text=Task already exists');
-     const target = page.locator('.swal2-html-container');
-     await expect(target).toHaveText('Task already exists!');
+  // const target = page.locator('.swal2-html-container >> text=Task already exists');
+  const target = page.locator(".swal2-html-container");
+  await expect(target).toHaveText("Task already exists!");
 
-     await expect(target).toBeVisible();
+  await expect(target).toBeVisible();
+
+  await request.delete(`http://localhost:3333/helper/tasks/${taskName}`);
 });
 
 //em testes automatizados, evite usar dados dinamicos como faker, pois se o teste falhar, fica dificil debugar
@@ -49,21 +54,56 @@ test('should not create a task that exists', async({ page}) => {
 //assim, cada teste pode ser executado isoladamente, sem depender de outros testes
 //isso tambem facilita a execucao paralela dos testes
 
-test('should create a new task, and should not create a task that exists', async({ page}) => {
-    const taskName = 'New Task from Playwright2.0';
-    await page.goto('http://localhost:8080');
+test("should create a new task, and should not create a task that exists", async ({
+  page,
+  request,
+}) => {
+  const taskName = "New Task from Playwright2.0";
+  await page.goto("http://localhost:8080");
 
-    const inputTaskName = page.locator('#newTask');
-    await inputTaskName.fill(taskName);
-    await page.click('css=button >> text=Create'); 
-    const target = page.locator('css=.task-item p >> text=', { hasText: taskName });
-    await expect(target).toBeVisible();
-     await inputTaskName.fill(taskName);
+  const inputTaskName = page.locator("#newTask");
+  await inputTaskName.fill(taskName);
+  await page.click("css=button >> text=Create");
+  const target = page.locator("css=.task-item p >> text=", {
+    hasText: taskName,
+  });
+  await expect(target).toBeVisible();
+  await inputTaskName.fill(taskName);
 
-    
-    await page.click('css=button >> text=Create'); 
-     const error = page.locator('.swal2-html-container');
-     await expect(error).toHaveText('Task already exists!');
-     await expect(error).toBeVisible();
-     await expect(target).toBeVisible();
+  await page.click("css=button >> text=Create");
+  const errorModal = page.locator(".swal2-html-container");
+  await expect(errorModal).toHaveText("Task already exists!");
+  await expect(errorModal).toBeVisible();
+  await expect(target).toBeVisible();
+  await request.delete(`http://localhost:3333/helper/tasks/${taskName}`);
+}); 
+
+test("should create a new task, and should not create a task that exists (with api call)", async ({
+  page,
+  request,
+}) => {
+  const task = {
+    name: "New Task from Playwright 3.0",
+    is_done: false,
+  };
+
+  await request.delete("http://localhost:3333/helper/tasks/" + task.name);
+
+  const newTask = await request.post("http://localhost:3333/tasks", {
+    data: task,
+  });
+  expect(newTask.ok()).toBeTruthy();
+  expect(newTask.status()).toBe(201);
+
+  await page.goto("http://localhost:8080");
+
+  const inputTaskName = page.locator("#newTask");
+  await inputTaskName.fill(task.name);
+  await page.click("css=button >> text=Create");
+
+  const errorModal = page.locator(".swal2-html-container");
+  await expect(errorModal).toBeVisible();
+  await expect(errorModal).toHaveText("Task already exists!");
+
+  await request.delete(`http://localhost:3333/helper/tasks/${task.name}`);
 });
